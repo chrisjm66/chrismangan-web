19575d6222e3c76d192fd957e0a29efde11d8c16
Chris Mangan <chrisjm66@gmail.com>
2026-08-16 13:00:53 -0400

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<div class="landing">
+  <div>
+    <h1>Chris Mangan</h1>
+    <div>
+      <a href="https://www.linkedin.com/in/manganchris/" target="_blank" rel="noopener noreferrer"
+        >LinkedIn</a
+      >
+      <a href="https://github.com/chrisjm66" target="_blank" rel="noopener noreferrer"
+        >GitHub</a
+      >
+    </div>
+  </div>
+</div>
